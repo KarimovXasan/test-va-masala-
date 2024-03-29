a1045949b4ce12b3b38917ef35b90da45fcd8e39
XasanboyKarimov <xkarimov257@gmail.com>
2024-03-29 15:29:23 +0500

    test va masala

---
+++ main.js
@@ -0,0 +1,28 @@
+//1 masala  
+
+// let a = 2;
+// let b = 5;
+
+// if(a > b){
+//     console.log("a katta son");
+// } else{
+//     console.log("b katta son");
+// }
+
+//3 masala
+
+// let num = 6;
+// if(num % 2 == 0){
+//     console.log("Siz juft son kiritingiz");
+// } else{
+//     console.log("siz tog son kiritingiiz");
+// }
+
+//6 masala
+
+for(let i = 20; i >= 2; i--){
+    console.log(i);
+}
+
+//5 masal
+
